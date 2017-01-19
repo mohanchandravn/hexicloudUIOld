56b039038bab94f5a62a063fb47d1cd6d61fac8d
vaduri <vaduri@VADURI-LAP.in.oracle.com>
2017-01-19 18:23:18 +0530

    committing mini guided path & mini services changes

--- HexiCloud/public_html/pages/guidedPathsMini/guidedPathsMini.js
+++ HexiCloud/public_html/pages/guidedPathsMini/guidedPathsMini.js
@@ -4,4 +4,53 @@
  * and open the template in the editor.
  */
 
+/**
+ * Guided path module
+ */
+define(['jquery','knockout','ojs/ojcore', 'ojs/ojprogressbar'
+], function (oj, ko) {
+    /**
+     * The view model for the main content view template
+     */
+    function serviceContentViewModel() {
+        var self = this;
+        
+        console.log('guided path page');
+        
+        self.srunningCPUCount = ko.observable(1);
+        self.stotalCPUCount = ko.observable(5);
+        self.scurrentUsedMemory = ko.observable(30);
+        self.sservicesArray = ko.observableArray([]);
+        self.sguidedPathsArray = ko.observableArray([]);
+        
+        self.getServiceDetails = function() {
+//            $.getJSON("pages/servicesMini/servicesMini.json", function(result) {
+//                self.sservicesArray([]);
+//                self.sservicesArray(result.services);
+//            });
+            $.getJSON("pages/guidedPathsMini/guidedPathsMini.json", function(result) {
+                self.sguidedPathsArray([]);
+                self.sguidedPathsArray(result.guidedPaths);
+            });
+        };
+        
+        self.handleAttached = function() {
+            self.getServiceDetails();
+        };
+        
+        self.routeTo = function(data, event) {
+            var id = event.currentTarget.id.toLowerCase();
+            router.go(id);
+        };
+        
+        self.logout = function(data, event) {
+            router.go('home/');
+        };
+    }
+    
+    
+    return serviceContentViewModel;
+});
+
+
 
--- HexiCloud/public_html/pages/servicesMini/servicesMini.js
+++ HexiCloud/public_html/pages/servicesMini/servicesMini.js
@@ -3,5 +3,58 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * service module
+ */
+define(['jquery','knockout','ojs/ojcore', 'ojs/ojprogressbar'
+], function (oj, ko) {
+    /**
+     * The view model for the main content view template
+     */
+    function serviceContentViewModel() {
+        var self = this;
+        
+        console.log('guided path page');
+        
+        self.srunningCPUCount = ko.observable(1);
+        self.stotalCPUCount = ko.observable(5);
+        self.scurrentUsedMemory = ko.observable(30);
+        self.sservicesArray = ko.observableArray([]);
+        self.sguidedPathsArray = ko.observableArray([]);
+        
+        self.getServiceDetails = function() {
+            $.getJSON("pages/servicesMini/servicesMini.json", function(result) {
+                self.sservicesArray([]);
+                self.sservicesArray(result.services);
+            });
+//            $.getJSON("pages/service/fullGuidedPathsDisplay.json", function(result) {
+//                self.sguidedPathsArray([]);
+//                self.sguidedPathsArray(result.guidedPaths);
+//            });
+        };
+        
+        self.handleAttached = function() {
+            self.getServiceDetails();
+        };
+        
+        self.routeTo = function(data, event) {
+            var id = event.currentTarget.id.toLowerCase();
+            router.go(id);
+        };
+        
+        self.logout = function(data, event) {
+            router.go('home/');
+        };
+    }
+    
+    
+    return serviceContentViewModel;
+});
 
 
