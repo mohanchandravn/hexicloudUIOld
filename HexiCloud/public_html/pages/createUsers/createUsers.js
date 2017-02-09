d39ac77e4087214897a83c447435c9e1b4a80be7
Nagaraju Yasarapu <nagaraju.yasarapu@oracle.com>
2017-02-09 16:54:00 +0530

    Added ojAccordion..@createUsers page

--- HexiCloud/public_html/pages/createUsers/createUsers.js
+++ HexiCloud/public_html/pages/createUsers/createUsers.js
@@ -7,7 +7,7 @@
 /**
  * login module
  */
-define(['knockout', 'config/serviceConfig', 'ojs/ojcore', 'jquery', , 'ojs/ojinputtext'
+define(['knockout', 'config/serviceConfig', 'ojs/ojcore', 'jquery', , 'ojs/ojinputtext', 'ojs/ojaccordion', 'ojs/ojcollapsible'
 ], function (ko, service) {
     /**
      * The view model for the main content view template
@@ -19,16 +19,24 @@ define(['knockout', 'config/serviceConfig', 'ojs/ojcore', 'jquery', , 'ojs/ojinp
         self.urlForAddingUsersAndAssigningRoles = ko.observable('http://docs.oracle.com/en/cloud/get-started/subscriptions-cloud/csgsg/adding-users-and-assigning-roles.html');
         self.textForAddingUsersAndAssigningRoles = ko.observable('How to add users - adding Users and Assigning Roles');
         
-        if (userRole() === 'itAdmin') {
-            self.pdfSrc = ko.observable('https://documents-gse00002841.documents.us2.oraclecloud.com/documents/link/LDFC37ABC5CD3EF8CBE505C7704A23FFF5A54B6D86DA/file/D68745A6BB6089B3199A8163704A23FFF5A54B6D86DA');
-        } else {
-            self.pdfSrc = ko.observable('https://documents-gse00002841.documents.us2.oraclecloud.com/documents/link/LDCDFF97F6E7411D4416C588704A23FFF5A54B6D86DA/file/DD9ADEC488FEEDE3210A732A704A23FFF5A54B6D86DA');
-        }
-        
         //for adding Admin Users and Assigning roles
         self.urlForAddingAdminUsersAndAssigningRoles = ko.observable('http://docs.oracle.com/en/cloud/get-started/subscriptions-cloud/csgsg/adding-users-and-assigning-roles.html');
         self.textForAddingAdminUsersAndAssigningRoles = ko.observable('How to add Account Admins - adding Users and Assigning Roles');
-                
+        
+        self.documentsArray = ko.observableArray([]);
+        
+        if (userRole() === 'itAdmin') {
+            for (var idx = 0; idx < 5; idx++) {
+                self.documentsArray.push({"docName": "Document " + (idx + 1), "pdfSrc": "https://documents-gse00002841.documents.us2.oraclecloud.com/documents/link/LDFC37ABC5CD3EF8CBE505C7704A23FFF5A54B6D86DA/file/D68745A6BB6089B3199A8163704A23FFF5A54B6D86DA"});
+            }
+//            self.pdfSrc = ko.observable('https://documents-gse00002841.documents.us2.oraclecloud.com/documents/link/LDFC37ABC5CD3EF8CBE505C7704A23FFF5A54B6D86DA/file/D68745A6BB6089B3199A8163704A23FFF5A54B6D86DA');
+        } else {
+            for (var idx = 0; idx < 5; idx++) {
+                self.documentsArray.push({"docName": "Document " + (idx + 1), "pdfSrc": "https://documents-gse00002841.documents.us2.oraclecloud.com/documents/link/LDCDFF97F6E7411D4416C588704A23FFF5A54B6D86DA/file/DD9ADEC488FEEDE3210A732A704A23FFF5A54B6D86DA"});
+            }
+//            self.pdfSrc = ko.observable('https://documents-gse00002841.documents.us2.oraclecloud.com/documents/link/LDCDFF97F6E7411D4416C588704A23FFF5A54B6D86DA/file/DD9ADEC488FEEDE3210A732A704A23FFF5A54B6D86DA');
+        }
+        
         self.goToServices = function(data, event) {
             isLoggedInUser(true);
             router.go('servicesMini/');
