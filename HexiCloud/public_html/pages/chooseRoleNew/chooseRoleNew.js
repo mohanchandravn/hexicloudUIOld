049bfbdf2196886cdf1348f9dde5251e0ddd5222
sgunjur <sgunjur@SGUNJUR-IN2.idc.oracle.com>
2017-03-02 12:17:28 +0530

    dnx ui changes - few more changes as 2/3/17 of  choose role functionality

--- HexiCloud/public_html/pages/chooseRoleNew/chooseRoleNew.js
+++ HexiCloud/public_html/pages/chooseRoleNew/chooseRoleNew.js
@@ -8,10 +8,10 @@ define(['ojs/ojcore',
             self.headerTitle = "Please confirm your role:";
             self.buyerTitle = "Buyer »";
             self.identityDomainAdminTitle = "Identity Domain Admin »";
-            self.welcomeUserMessage = ko.observable("Hello");
+            self.welcomeUserMessage = ko.observable("Welcome, Firstname");
             if(loggedInUser())
             {
-                self.welcomeUserMessage(self.welcomeUserMessage()+" "+loggedInUser());
+                self.welcomeUserMessage(self.welcomeUserMessage()+loggedInUser());
             }
             var router = params.ojRouter.parentRouter;
             self.buyerSelect = function ()
@@ -27,7 +27,7 @@ define(['ojs/ojcore',
                 
             };
             self.identityDomainAdminSelect = function (){
-                loggedInUserRole('accountAdmin');
+                loggedInUserRole('itAdmin');
                 $('.blur-node1, .blur-node2').addClass('animate');
                 setTimeout(function(){
                     router.go('/addAnother');
